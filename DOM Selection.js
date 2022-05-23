498351a4c9a2a31105de1e61a6d1136d4ea7c182
Adanpradana <adanizkahariz@gmail.com>
2022-05-23 20:34:20 +0700

    add file

--- DOM Selection.js
+++ DOM Selection.js
@@ -8,10 +8,10 @@ judul.innerHTML = "HAHAKABE";
 // document.getElementByTagName()
 // return HTMLCollection;
 // menggunakan array maka elemen menggunakan [i]
-const p = document.getElementsByTagName("p");
-for (let i = 0; i < p.length; i++) {
-  p[i].style.fontFamily = "monospace";
-}
+// const p = document.getElementsByTagName("p");
+// for (let i = 0; i < p.length; i++) {
+//   p[i].style.fontFamily = "monospace";
+// }
 
 // menghasilkan HTML Collection
 const h1 = document.getElementsByTagName("h1")[0];
@@ -22,3 +22,20 @@ h1.style.fontSize = "50px";
 // inner HTML atau mengganti elemen
 const p1 = document.getElementsByClassName("p1");
 p1[0].innerHTML = "Ini Adalah Paragraph";
+
+// document.querrySelector() -> ELEMENT
+
+const p4 = document.querySelector("#b p");
+p4.style.color = "white";
+p4.style.backgroundColor = "red";
+p4.style.fontSize = "25px";
+
+const li2 = document.querySelector("#b ul li:nth-child(3)");
+li2.style.backgroundColor = "salmon";
+
+const p = document.querySelectorAll("p");
+
+for (let i = 0; i < p.length; i++) {
+  p[i].style.backgroundColor = "salmon";
+  p[i].style.color = "white";
+}
